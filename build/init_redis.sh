0b95fad508bdc231f5a14f3f42a6bcc2809e0a12
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2023-05-05 00:00:02 +0800

    fix: redis connect (#2)
    * fix: redis connect
    
    * chore: add Readme and support no tls
    
    * chore: cache docker build
    
    * chore: update
    
    * fix: fix postgres and redis connection
    
    * chore: update port
    
    * ci: lint

--- build/init_redis.sh
+++ build/init_redis.sh
@@ -2,7 +2,7 @@
 set -x
 set -eo pipefail
 
-RUNNING_CONTAINER=$(docker ps --filter 'name=redis' --format '{{.ID}}')
+RUNNING_CONTAINER=$(docker ps --filter 'name=appflowy_redis' --format '{{.ID}}')
 if [[ -n $RUNNING_CONTAINER ]]; then
   echo >&2 "there is a redis container already running, kill it with"
   echo >&2 "    docker kill ${RUNNING_CONTAINER}"
@@ -13,7 +13,7 @@ fi
 docker run \
     -p "6379:6379" \
     -d \
-    --name "redis_$(date '+%s')" \
+    --name "appflowy_redis_$(date '+%s')" \
     redis:7
 
 >&2 echo "Redis is ready to go!"

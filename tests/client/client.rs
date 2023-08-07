b2c3ec6f3491523253ed1c28411b0ad5b5ed3af4
Fu Zi Xiang <speed2exe@live.com.sg>
2023-08-07 11:00:52 +0000

    added rest of register test case

--- tests/client/client.rs
+++ tests/client/client.rs
@@ -14,6 +14,31 @@ async fn register_success() {
   assert!(token.len() > 0);
 }
 
+#[tokio::test]
+async fn register_with_invalid_password() {
+  let c = client::Client::from(reqwest::Client::new(), LOCALHOST_URL);
+  let email = format!("deep_fake{}@appflowy.io", timestamp_nano());
+  let token = c.register("user1", &email, "123").await;
+  assert!(token.is_err());
+}
+
+#[tokio::test]
+async fn register_with_invalid_name() {
+  let c = client::Client::from(reqwest::Client::new(), LOCALHOST_URL);
+  let email = format!("deep_fake{}@appflowy.io", timestamp_nano());
+  let token = c.register("", &email, "DeepFakePassword!123").await;
+  assert!(token.is_err());
+}
+
+#[tokio::test]
+async fn register_with_invalid_email() {
+  let c = client::Client::from(reqwest::Client::new(), LOCALHOST_URL);
+  let token = c
+    .register("user1", "appflowy.io", "DeepFakePassword!123")
+    .await;
+  assert!(token.is_err());
+}
+
 // Utils
 fn timestamp_nano() -> u128 {
   SystemTime::now()

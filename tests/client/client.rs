5250d9e67c8fb87f77eceb0574fbc7b9ac35cf4e
Fu Zi Xiang <speed2exe@live.com.sg>
2023-08-07 10:47:35 +0000

    fix: use time in email for indempotency

--- tests/client/client.rs
+++ tests/client/client.rs
@@ -1,13 +1,23 @@
 use appflowy_server::client::client;
+use std::time::SystemTime;
 
 const LOCALHOST_URL: &str = "http://127.0.0.1:8000"; //TODO: change to default port
 
 #[tokio::test]
 async fn register_success() {
   let c = client::Client::from(reqwest::Client::new(), LOCALHOST_URL);
+  let email = format!("deep_fake{}@appflowy.io", timestamp_nano());
   let token = c
-    .register("user1", "deep_fake4@appflowy.io", "DeepFakePassword!123")
+    .register("user1", &email, "DeepFakePassword!123")
     .await
     .unwrap();
   assert!(token.len() > 0);
+}
+
+// Utils
+fn timestamp_nano() -> u128 {
+  SystemTime::now()
+    .duration_since(SystemTime::UNIX_EPOCH)
+    .unwrap()
+    .as_nanos()
 }

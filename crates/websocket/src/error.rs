e65b6333b1d46bf46187d8fca150ac46a18308de
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2023-08-08 16:13:18 +0800

    Feat: multi ws conn (#6)
    * feat: multi conn
    
    * fix: multi connect

--- crates/websocket/src/error.rs
+++ crates/websocket/src/error.rs
@@ -1,8 +1,5 @@
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, Clone, thiserror::Error)]
 pub enum WSError {
-  #[error(transparent)]
-  Persistence(#[from] collab_plugins::disk::error::PersistenceError),
-
-  #[error("Internal failure: {0}")]
-  Internal(#[from] Box<dyn std::error::Error + Send + Sync>),
+  #[error("Internal failure:{0}")]
+  Internal(String),
 }

d50de4e4d8a97132e370c05270fb4775aca0c4cb
Zack <33050391+speed2exe@users.noreply.github.com>
2023-08-11 08:43:21 +0800

    feat: added capability to decode supabase web token (#8)

---
+++ src/component/auth/supabase_jwt.rs
@@ -0,0 +1,61 @@
+use anyhow::Error;
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+use serde::{Deserialize, Serialize};
+
+lazy_static::lazy_static! {
+  pub static ref VALIDATION: Validation = Validation::new(Algorithm::HS256);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Token {
+  Anonymous(Anonymous),
+  Authenticated(Box<Authenticated>),
+}
+
+impl Token {
+  #[allow(dead_code)]
+  pub fn decode_from_str(&self, token: &str, secret: &[u8]) -> Result<Token, Error> {
+    let token_data = decode::<Token>(token, &DecodingKey::from_secret(secret), &VALIDATION)?;
+    Ok(token_data.claims)
+  }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Authenticated {
+  aud: String,
+  exp: u64,
+  iat: u64,
+  iss: String,
+  sub: String,
+  email: String,
+  phone: String,
+  app_metadata: AppMetadata,
+  user_metadata: std::collections::HashMap<String, String>, // or another struct if you know the fields
+  role: String,
+  aal: String,
+  amr: Vec<Amr>,
+  session_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Anonymous {
+  iss: String,
+  #[serde(rename = "ref")]
+  reference: String,
+  role: String,
+  iat: u64,
+  exp: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct AppMetadata {
+  provider: String,
+  providers: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Amr {
+  method: String,
+  timestamp: u64,
+}

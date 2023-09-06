4eccc52106a8e76d7af92af49ac09c2920e9ced1
Fu Zi Xiang <speed2exe@live.com.sg>
2023-09-06 10:59:29 +0800

    fix: test: generate unique email for registration to avoid conflict

--- tests/client/sign_up.rs
+++ tests/client/sign_up.rs
@@ -1,27 +1,30 @@
-use crate::client::constants::LOCALHOST_URL;
+use crate::client::{constants::LOCALHOST_URL, utils::generate_unique_email};
 use appflowy_cloud::client::http::Client;
 
 #[tokio::test]
 async fn sign_up_success() {
+  let email = generate_unique_email();
+  let password = "Hello!123#";
   let c = Client::from(reqwest::Client::new(), LOCALHOST_URL);
-  let resp = c
-    .sign_up("exampleuser@appflowy.io", "Hello!123#")
-    .await
-    .unwrap();
+  let resp = c.sign_up(&email, password).await.unwrap();
   assert!(resp.confirmation_sent_at.is_some());
   assert!(resp.confirmed_at.is_none());
 }
 
 #[tokio::test]
 async fn sign_up_invalid_email() {
+  let invalid_email = "not_email_address";
+  let password = "Hello!123#";
   let c = Client::from(reqwest::Client::new(), LOCALHOST_URL);
-  let resp = c.sign_up("exampleuser", "Hello!123#").await;
+  let resp = c.sign_up(invalid_email, password).await;
   assert!(resp.is_err());
 }
 
 #[tokio::test]
 async fn sign_up_invalid_password() {
+  let email = generate_unique_email();
+  let password = "123";
   let c = Client::from(reqwest::Client::new(), LOCALHOST_URL);
-  let resp = c.sign_up("exampleuser@appflowy.io", "123").await;
+  let resp = c.sign_up(&email, password).await;
   assert!(resp.is_err());
 }

73d42791c5f96f899d3ee5b1a0b6e33d29bc4392
nathan <nathan@appflowy.io>
2023-09-08 20:20:20 +0800

    chore: remove redundant sqlx command

--- build/run_local_server.sh
+++ build/run_local_server.sh
@@ -4,7 +4,17 @@ set -eo pipefail
 
 cd "$(dirname "$0")/.."
 
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_PORT="${POSTGRES_PORT:=5433}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+DB_NAME="${POSTGRES_DB:=postgres}"
+
 docker-compose --file ./docker-compose-dev.yml up -d --build
-sqlx database create
-sqlx migrate run
+
+# Keep pinging Postgres until it's ready to accept commands
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
 cargo run

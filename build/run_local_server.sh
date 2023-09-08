67e11d9bd5a17fd4b208b424f04f29a400fc4cfb
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2023-09-08 20:35:06 +0800

    fix: run_local_server script (#21)

--- build/run_local_server.sh
+++ build/run_local_server.sh
@@ -4,7 +4,18 @@ set -eo pipefail
 
 cd "$(dirname "$0")/.."
 
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_PORT="${POSTGRES_PORT:=5433}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
 docker-compose --file ./docker-compose-dev.yml up -d --build
+
+# Keep pinging Postgres until it's ready to accept commands
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
 sqlx database create
 sqlx migrate run
 cargo run

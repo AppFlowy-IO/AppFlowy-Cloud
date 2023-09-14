c34309d88cf3f8467a3f07b840fac6fca1da0da1
Fu Zi Xiang <speed2exe@live.com.sg>
2023-09-14 14:38:15 +0800

    fix: cargo clippy

--- tests/client/refresh.rs
+++ tests/client/refresh.rs
@@ -8,6 +8,6 @@ async fn refresh_success() {
   let email = &REGISTERED_EMAIL;
   let password = &REGISTERED_PASSWORD;
   let mut c = client_api_client();
-  c.sign_in_password(&email, password).await.unwrap();
+  c.sign_in_password(email, password).await.unwrap();
   c.refresh().await.unwrap();
 }

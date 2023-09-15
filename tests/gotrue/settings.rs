3c598f178ed5ea2ec18a916b910581403ea66380
Zack <33050391+speed2exe@users.noreply.github.com>
2023-09-15 21:13:43 +0800

    ci: test env docker-compose deployment (#48)
    * ci: test env docker-compose deployment
    
    * ci: test deployment
    
    * fix: replace delimiter with pipe to avoid conflict
    
    * fix: API_EXTERNAL_URL_TEST from secrets
    
    * chore: remove unneeded deploy when pull request
    
    * fix: cloud public host name/address
    
    * chore: trigger: change to push
    
    * chore: add ext_url for exposed gotrue auth server

--- tests/gotrue/settings.rs
+++ tests/gotrue/settings.rs
@@ -3,6 +3,10 @@ use gotrue::api::Client;
 #[tokio::test]
 async fn gotrue_settings() {
   let http_client = reqwest::Client::new();
-  let gotrue_client = Client::new(http_client, "http://localhost:9998");
+  let gotrue_client = Client::new(
+    http_client,
+    "http://localhost:9998",
+    "http://localhost:9998",
+  );
   gotrue_client.settings().await.unwrap();
 }

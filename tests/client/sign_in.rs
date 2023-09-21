306988ae60e69b59799d0716878b78914d6943f2
Fu Zi Xiang <speed2exe@live.com.sg>
2023-09-21 11:37:00 +0800

    chore: remove uneeded use

--- tests/client/sign_in.rs
+++ tests/client/sign_in.rs
@@ -1,6 +1,6 @@
 use shared_entity::error_code::ErrorCode;
 
-use crate::client::utils::{generate_unique_email, REGISTERED_USERS, REGISTERED_USERS_MUTEX};
+use crate::client::utils::{generate_unique_email, REGISTERED_USERS};
 use crate::client_api_client;
 
 #[tokio::test]

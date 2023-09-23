e3ff7651374d1e8545e2ac99f78438a37e5cf2c6
Zack <33050391+speed2exe@users.noreply.github.com>
2023-09-23 20:06:37 +0800

    feat: Gotrue admin api (#74)
    * feat: set up admin account and auto confirm during appflowy start
    
    * feat: client auth against gotrue whenever possible
    
    * feat: admin add user
    
    * feat: implement admin add user
    
    * feat: generate registered user
    
    * fix: enable cloud feature for client_api
    
    * fix: test same user fix
    
    * feat: added admin magic link generation

--- libs/gotrue/src/params.rs
+++ libs/gotrue/src/params.rs
@@ -1,5 +1,6 @@
 use std::collections::btree_map::BTreeMap;
 
+use gotrue_entity::{Factor, Identity};
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Deserialize, Serialize)]
@@ -14,4 +15,69 @@ pub struct AdminUserParams {
   pub user_metadata: BTreeMap<String, serde_json::Value>,
   pub app_metadata: BTreeMap<String, serde_json::Value>,
   pub ban_duration: String,
+}
+
+#[derive(Default, Deserialize, Serialize)]
+pub struct GenerateLinkParams {
+  #[serde(rename = "type")]
+  pub type_: GenerateLinkType,
+
+  pub email: String,
+  pub new_email: String,
+  pub password: String,
+  pub data: BTreeMap<String, serde_json::Value>,
+  pub redirect_to: String,
+}
+
+#[derive(Default, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum GenerateLinkType {
+  #[default]
+  MagicLink,
+
+  Recovery,
+  Invite,
+  Signup,
+  EmailChange,
+  PhoneChange,
+  Reauthenticate,
+  Sms,
+  Email,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GenerateLinkResponse {
+  // putting User here as Rust does not support struct field extension
+  // use gotrue_entity::User
+  pub id: String,
+  pub aud: String,
+  pub role: String,
+  pub email: String,
+  pub email_confirmed_at: Option<String>,
+  pub invited_at: Option<String>,
+  pub phone: String,
+  pub phone_confirmed_at: Option<String>,
+  pub confirmation_sent_at: Option<String>,
+  pub confirmed_at: Option<String>,
+  pub recovery_sent_at: Option<String>,
+  pub new_email: Option<String>,
+  pub email_change_sent_at: Option<String>,
+  pub new_phone: Option<String>,
+  pub phone_change_sent_at: Option<String>,
+  pub reauthentication_sent_at: Option<String>,
+  pub last_sign_in_at: Option<String>,
+  pub app_metadata: serde_json::Value,
+  pub user_metadata: serde_json::Value,
+  pub factors: Option<Vec<Factor>>,
+  pub identities: Vec<Identity>,
+  pub created_at: String,
+  pub updated_at: String,
+  pub banned_until: Option<String>,
+  pub deleted_at: Option<String>,
+  //
+  pub action_link: String,
+  pub email_otp: String,
+  pub hashed_token: String,
+  pub verification_type: String,
+  pub redirect_to: String,
 }

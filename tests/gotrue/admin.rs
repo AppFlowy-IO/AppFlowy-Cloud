0092fc34cf1c48c96a9c5782aa2bc2173deb8821
Fu Zi Xiang <speed2exe@live.com.sg>
2023-09-23 18:19:48 +0800

    feat: added admin magic link generation

---
+++ tests/gotrue/admin.rs
@@ -0,0 +1,110 @@
+use gotrue::{
+  api::Client,
+  grant::{Grant, PasswordGrant},
+  params::{AdminUserParams, GenerateLinkParams},
+};
+use scraper::{Html, Selector};
+
+use crate::{
+  client::{
+    constants::LOCALHOST_GOTRUE,
+    utils::{generate_unique_email, ADMIN_USER},
+  },
+  client_api_client,
+};
+
+#[tokio::test]
+async fn admin_user_create() {
+  let http_client = reqwest::Client::new();
+  let gotrue_client = Client::new(http_client, "http://localhost:9998");
+  let admin_token = gotrue_client
+    .token(&Grant::Password(PasswordGrant {
+      email: ADMIN_USER.email.clone(),
+      password: ADMIN_USER.password.clone(),
+    }))
+    .await
+    .unwrap();
+
+  // new user params
+  let user_email = generate_unique_email();
+  let user_password = "Hello123!";
+
+  // create user
+  let admin_user_params: AdminUserParams = AdminUserParams {
+    email: user_email.clone(),
+    password: Some(user_password.to_string()),
+    email_confirm: true,
+    ..Default::default()
+  };
+  let user = gotrue_client
+    .admin_add_user(&admin_token.access_token, &admin_user_params)
+    .await
+    .unwrap();
+  assert_eq!(user.email, user_email);
+  assert!(user.email_confirmed_at.is_some());
+
+  // login user
+  let user_token = gotrue_client
+    .token(&Grant::Password(PasswordGrant {
+      email: user_email.clone(),
+      password: user_password.to_string(),
+    }))
+    .await
+    .unwrap();
+  assert!(user_token.user.email_confirmed_at.is_some());
+}
+
+#[tokio::test]
+async fn admin_generate_link_and_user_sign_in() {
+  let http_client = reqwest::Client::new();
+  let gotrue_client = Client::new(http_client, LOCALHOST_GOTRUE);
+  let admin_token = gotrue_client
+    .token(&Grant::Password(PasswordGrant {
+      email: ADMIN_USER.email.clone(),
+      password: ADMIN_USER.password.clone(),
+    }))
+    .await
+    .unwrap();
+
+  // new user params
+  let user_email = generate_unique_email();
+
+  // create link
+  let admin_user_params: GenerateLinkParams = GenerateLinkParams {
+    email: user_email.clone(),
+    ..Default::default()
+  };
+  let link_resp = gotrue_client
+    .generate_link(&admin_token.access_token, &admin_user_params)
+    .await
+    .unwrap();
+
+  assert_eq!(link_resp.email, user_email);
+
+  // visit action link
+  let action_link = link_resp.action_link;
+  let reqwest_client = reqwest::Client::new();
+  let resp = reqwest_client.get(action_link).send().await.unwrap();
+  let resp_text = resp.text().await.unwrap();
+  let appflowy_sign_in_url = extract_appflowy_sign_in_url(&resp_text);
+
+  let client = client_api_client();
+  let is_new = client.sign_in_url(&appflowy_sign_in_url).await.unwrap();
+  assert!(is_new);
+
+  let workspaces = client.workspaces().await.unwrap();
+  assert_eq!(workspaces.len(), 1);
+}
+
+fn extract_appflowy_sign_in_url(html_str: &str) -> String {
+  let fragment = Html::parse_fragment(html_str);
+  let selector = Selector::parse("a").unwrap();
+  fragment
+    .select(&selector)
+    .next()
+    .unwrap()
+    .value()
+    .attr("href")
+    .unwrap()
+    .to_string()
+}

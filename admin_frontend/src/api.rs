827b79e15cd95a8e82318fa5708fd8921b758323
Fu Zi Xiang <speed2exe@live.com.sg>
2023-10-09 17:07:16 +0800

    feat: session token WIP

--- admin_frontend/src/api.rs
+++ admin_frontend/src/api.rs
@@ -1,8 +1,16 @@
 use axum::{routing::post, Router};
+use leptos::{server, ServerFnError};
 
 pub fn api_router() -> Router {
-    Router::new().route("/test", post(test_handler))
+  Router::new().route("/test", post(test_handler))
 }
+
 pub async fn test_handler() -> String {
-    "Test from api".to_string()
+  "Test from api".to_string()
+}
+
+#[server(Foo)]
+pub async fn foo() -> Result<(), ServerFnError> {
+  println!("Foo!");
+  Ok(())
 }
--- admin_frontend/src/app.rs
+++ admin_frontend/src/app.rs
@@ -1,100 +1,129 @@
-use leptos::{component, view, IntoView};
+use crate::api::{foo, Foo};
+use leptos::{component, create_resource, create_server_action, provide_context, view, IntoView};
+use leptos_axum::handle_server_fns_with_context;
 use leptos_router::{Route, Router, Routes};
 
 #[component]
 pub fn App() -> impl IntoView {
-    view! {
-        <Router>
-            <Routes>
-                <Route path="/" view=Home/>
-                <Route path="/admin" view=Home/>
-                <Route path="/admin/settings" view=Settings/>
-                <Route path="/admin/users" view=Users/>
-            </Routes>
-        </Router>
-    }
+  view! {
+      <Router>
+          <Routes>
+              <Route path="/" view=Home/>
+              <Route path="/admin" view=Home/>
+              <Route path="/admin/settings" view=Settings/>
+              <Route path="/admin/users" view=Users/>
+          </Routes>
+      </Router>
+  }
 }
 
 #[component]
 pub fn Users() -> impl IntoView {
-    view! {
-        <script
-            src="https://unpkg.com/htmx.org@1.9.6"
-            integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni"
-            crossorigin="anonymous"
-        ></script>
-    }
+  view! {
+      <script
+          src="https://unpkg.com/htmx.org@1.9.6"
+          integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni"
+          crossorigin="anonymous"
+      ></script>
+  }
 }
 
 #[component]
 pub fn Home() -> impl IntoView {
-    view! {
-        <script
-            src="https://unpkg.com/htmx.org@1.9.6"
-            integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni"
-            crossorigin="anonymous"
-        ></script>
+  // let a = tokio::spawn(async move {
+  //   println!("Hello from tokio!");
+  //   tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+  //   println!("Hello from tokio again!");
+  // });
+  // let b = tokio::runtime::Runtime::new().unwrap();
+  // b.block_on(a).unwrap();
 
-        <body>
-            <h1>Admin Login</h1>
+  // let action = create_server_action::<Foo>();
+  // // create_resource(source, fetcher)
+  // let bar = create_resource(
+  //   move || {
+  //     (
+  //       // action.version().get(),
+  //       // action.version().post(),
+  //       // action.version().put(),
+  //       // action.version().delete(),
+  //     )
+  //   },
+  //   move |_| foo(),
+  // );
+  // println!("bar: {:?}", bar);
+  // handle_server_fns_with_context(path, headers, raw_query, additional_context, req)
 
-            <form>
-                <table>
-                    <tr>
-                        <td>
-                            <label for="email">Email:</label>
-                        </td>
-                        <td>
-                            <input type="text" id="email" name="email" required/>
-                        </td>
+  view! {
+      <script
+          src="https://unpkg.com/htmx.org@1.9.6"
+          integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni"
+          crossorigin="anonymous"
+      ></script>
 
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="password">Password:</label>
-                        </td>
-                        <td>
-                            <input type="password" id="password" name="password" required/>
-                        </td>
+      <body>
+          <h1>Admin Login</h1>
 
-                    </tr>
-                </table>
-                <button hx-post="/token?grant_type=password"
-                        hx-target="#response">
-                    Submit
-                </button>
-            </form>
-            <div id="response"></div>
-        </body>
-    }
+    let todos = create_resource(
+        move || (add_todo.version().get(), delete_todo.version().get()),
+        move |_| get_todos(),
+    );
+          <form>
+              <table>
+                  <tr>
+                      <td>
+                          <label for="email">Email:</label>
+                      </td>
+                      <td>
+                          <input type="text" id="email" name="email" required/>
+                      </td>
+
+                  </tr>
+                  <tr>
+                      <td>
+                          <label for="password">Password:</label>
+                      </td>
+                      <td>
+                          <input type="password" id="password" name="password" required/>
+                      </td>
+
+                  </tr>
+              </table>
+              <button hx-get="/settings" hx-target="#response">
+                  Submit
+              </button>
+          </form>
+          <div id="response"></div>
+      </body>
+  }
 }
 
 #[component]
 pub fn Settings() -> impl IntoView {
-    view! {
-        <script
-            src="https://unpkg.com/htmx.org@1.9.6"
-            integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni"
-            crossorigin="anonymous"
-        ></script>
-        <h1>Settings Page</h1>
-        <button
-            hx-post="https://test.appflowy.cloud/settings"
-            hx-trigger="click"
-            hx-swap="innerHTML"
-            hx-target="#content"
-            mustache-template="foo"
-        >
-            >
-                Click Me!
-        </button>
+  view! {
+      <script
+          src="https://unpkg.com/htmx.org@1.9.6"
+          integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni"
+          crossorigin="anonymous"
+      ></script>
+      <h1>Settings Page</h1>
+      <button
+          hx-post="https://test.appflowy.cloud/settings"
+          hx-trigger="click"
+          hx-swap="innerHTML"
+          hx-target="#content"
+          mustache-template="foo"
+      >
+          >
+              Click Me!
+      </button>
 
-        <p id="content">Start</p>
-        <template id="foo"></template>
+      <p id="content">Start</p>
+      <template id="foo"></template>
 
-        <nav>
-            <h2>"Navigation"</h2>
-            <a href="/">"/home"</a>
-        </nav>
-    }
+      <nav>
+          <h2>"Navigation"</h2>
+          <a href="/">"/home"</a>
+      </nav>
+  }
 }

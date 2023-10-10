427612b11fd3918f29ba646ccc681edea536346d
Fu Zi Xiang <speed2exe@live.com.sg>
2023-10-10 13:02:41 +0800

    feat: added logout

--- admin_frontend/src/main.rs
+++ admin_frontend/src/main.rs
@@ -5,7 +5,7 @@ mod templates;
 mod web_api;
 mod web_app;
 
-use axum::Router;
+use axum::{response::Redirect, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
@@ -19,12 +19,13 @@ async fn main() {
 
   let state = AppState { gotrue_client };
 
-  let web_api_router = web_api::router().with_state(state.clone());
   let web_app_router = web_app::router();
+  let web_api_router = web_api::router().with_state(state);
 
   let app = Router::new()
-    .nest_service("/web-api", web_api_router)
-    .nest_service("/", web_app_router);
+    .route("/", get(|| async { Redirect::permanent("/web") }))
+    .nest_service("/web", web_app_router)
+    .nest_service("/web-api", web_api_router);
 
   axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
     .serve(app.into_make_service())
--- admin_frontend/src/templates.rs
+++ admin_frontend/src/templates.rs
@@ -3,3 +3,7 @@ use askama::Template;
 #[derive(Template)]
 #[template(path = "login.html")]
 pub struct Login;
+
+#[derive(Template)]
+#[template(path = "home.html")]
+pub struct Home;

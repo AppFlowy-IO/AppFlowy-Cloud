57e6d07c5a88dc05ca11c943d3b9b8718bbd5b6d
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2023-10-22 19:21:23 +0800

    feat: insert user metadata (#131)

--- libs/shared-entity/src/dto/auth_dto.rs
+++ libs/shared-entity/src/dto/auth_dto.rs
@@ -1,11 +1,28 @@
 // Data Transfer Objects (DTO)
 
 use gotrue_entity::AccessTokenResponse;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(Deserialize, Serialize)]
 pub struct SignInParams {
   pub email: String,
   pub password: String,
+}
+
+#[derive(Default, Deserialize, Serialize, Clone)]
+pub struct UserMetaData(HashMap<String, serde_json::Value>);
+impl UserMetaData {
+  pub fn new() -> Self {
+    Self::default()
+  }
+  pub fn into_inner(self) -> HashMap<String, serde_json::Value> {
+    self.0
+  }
+
+  pub fn insert<T: Into<serde_json::Value>>(&mut self, key: &str, value: T) {
+    self.0.insert(key.to_string(), value.into());
+  }
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Default)]
@@ -13,25 +30,27 @@ pub struct UpdateUsernameParams {
   pub name: Option<String>,
   pub password: Option<String>,
   pub email: Option<String>,
+  pub metadata: Option<UserMetaData>,
 }
 
 impl UpdateUsernameParams {
   pub fn new() -> Self {
     Self::default()
   }
-
   pub fn with_password<T: ToString>(mut self, password: T) -> Self {
     self.password = Some(password.to_string());
     self
   }
-
   pub fn with_name<T: ToString>(mut self, name: T) -> Self {
     self.name = Some(name.to_string());
     self
   }
-
   pub fn with_email<T: ToString>(mut self, email: T) -> Self {
     self.email = Some(email.to_string());
+    self
+  }
+  pub fn with_metadata<T: Into<UserMetaData>>(mut self, metadata: T) -> Self {
+    self.metadata = Some(metadata.into());
     self
   }
 }

9b33ed67a472b9e607bd6046922e525a6e6f7776
nathan <nathan@appflowy.io>
2024-01-05 01:46:05 +0800

    chore: add ci check

---
+++ build/client_api_deps_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Generate the current dependency list
+cargo tree > current_deps.txt
+
+BASELINE_COUNT=547
+CURRENT_COUNT=$(cat current_deps.txt | wc -l)
+
+if [ "$CURRENT_COUNT" -gt "$BASELINE_COUNT" ]; then
+    echo "Dependency count has increased!"
+    exit 1
+else
+    echo "No increase in dependencies."
+fi

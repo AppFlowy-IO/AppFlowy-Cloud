29283ae1b2a45f7c7259230a66af853b8fd4564a
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-28 02:01:15 +0800

    chore: add flakes to gitignore

--- tests/workspace/workspace_crud.rs
+++ tests/workspace/workspace_crud.rs
@@ -1,4 +1,6 @@
 use client_api_test_util::generate_unique_registered_user_client;
+use database_entity::dto::AFRole;
+use shared_entity::dto::workspace_dto::CreateWorkspaceMember;
 use shared_entity::dto::workspace_dto::CreateWorkspaceParam;
 
 #[tokio::test]
@@ -28,4 +30,45 @@ async fn add_and_delete_workspace_for_user() {
     .unwrap();
   let workspaces = c.get_workspaces().await.unwrap();
   assert_eq!(workspaces.0.len(), 1);
+}
+
+#[tokio::test]
+async fn add_and_delete_workspace_for_non_owner_user() {
+  let (member, member_user) = generate_unique_registered_user_client().await;
+
+  // Owner added member to workspace
+  let (owner, _user) = generate_unique_registered_user_client().await;
+  let owner_workspace = owner
+    .create_workspace(CreateWorkspaceParam {
+      workspace_name: Some("owner_workspace".to_string()),
+    })
+    .await
+    .unwrap();
+
+  owner
+    .add_workspace_members(
+      owner_workspace.workspace_id.to_string(),
+      vec![CreateWorkspaceMember {
+        email: member_user.email.clone(),
+        role: AFRole::Member,
+      }],
+    )
+    .await
+    .unwrap();
+
+  // Member should have 2 workspaces
+  let member_workspaces = member.get_workspaces().await.unwrap();
+  assert_eq!(member_workspaces.0.len(), 2);
+
+  owner
+    .remove_workspace_members(
+      owner_workspace.workspace_id.to_string(),
+      vec![member_user.email],
+    )
+    .await
+    .unwrap();
+
+  // Member should have 1 workspaces, because owner removed him
+  let member_workspaces = member.get_workspaces().await.unwrap();
+  assert_eq!(member_workspaces.0.len(), 1);
 }

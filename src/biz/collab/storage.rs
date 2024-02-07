03471f3af7a1a8c2727367dec9e5fa4bad8d10a1
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2024-02-08 02:32:25 +0800

    chore: update feature of actix (#304)
    * chore: update feature of actix
    
    * chore: add logs

--- src/biz/collab/storage.rs
+++ src/biz/collab/storage.rs
@@ -84,6 +84,10 @@ where
     self.disk_cache.config()
   }
 
+  async fn status(&self) -> String {
+    format!("cache collab: {}", self.mem_cache.len().await)
+  }
+
   async fn cache_collab(&self, object_id: &str, collab: Weak<MutexCollab>) {
     tracing::trace!("cache opened collab:{}", object_id);
     self

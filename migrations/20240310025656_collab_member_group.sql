dc792caf635e7dae95f1e3f0fdf9eb7b74b5807e
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-03-10 17:25:42 +0800

    feat: improve trigger order

--- migrations/20240310025656_collab_member_group.sql
+++ migrations/20240310025656_collab_member_group.sql
@@ -21,18 +21,19 @@ CREATE TABLE IF NOT EXISTS af_collab_user_group (
 ALTER TABLE af_collab_member ADD COLUMN uid_type SMALLINT DEFAULT 0; -- 0: user, 1: group
 
 -- Trigger to create default public group when workspace is created
-CREATE OR REPLACE FUNCTION public_group_creation()
+CREATE OR REPLACE FUNCTION af_public_group_creation()
 RETURNS TRIGGER AS $$
 BEGIN
   INSERT INTO af_collab_group (workspace_id, creator_uid, guid, name)
   VALUES (NEW.workspace_id, NEW.owner_uid, '00000000-0000-0000-0000-000000000000', 'Public');
+  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER public_group_creation_trigger
+CREATE TRIGGER af_public_group_creation_trigger
 AFTER INSERT ON af_workspace
 FOR EACH ROW
-EXECUTE FUNCTION public_group_creation();
+EXECUTE FUNCTION af_public_group_creation();
 
 -- Trigger to add newly created member to public group when new member is added to workspace
 CREATE OR REPLACE FUNCTION add_member_to_public_group()
@@ -40,6 +41,7 @@ RETURNS TRIGGER AS $$
 BEGIN
   INSERT INTO af_collab_user_group (workspace_id, guid, uid)
   VALUES (NEW.workspace_id, '00000000-0000-0000-0000-000000000000', NEW.uid);
+  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 

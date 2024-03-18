e9e3552901ef0f0996a1734ba736184d9043de20
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-03-18 15:59:07 +0800

    feat: workspace invitation view shared and accept

--- admin_frontend/src/askama_entities.rs
+++ admin_frontend/src/askama_entities.rs
@@ -1,5 +1,8 @@
+use database_entity::dto::AFWorkspace;
+
+use crate::ext::entities::WorkspaceMember;
+
 pub struct WorkspaceWithMembers {
-  pub workspace_id: String,
-  pub workspace_name: String,
-  pub member_emails: Vec<String>,
+  pub workspace: AFWorkspace,
+  pub members: Vec<WorkspaceMember>,
 }

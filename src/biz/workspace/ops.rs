528dd6ae5b8b640900ac7d10a4901dd59a5450dc
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-03-21 14:01:48 +0800

    feat: email redirect to invite page

--- src/biz/workspace/ops.rs
+++ src/biz/workspace/ops.rs
@@ -21,7 +21,7 @@ use database_entity::dto::{
   WorkspaceUsage,
 };
 
-use gotrue::params::InviteUserParams;
+use gotrue::params::{InviteUserParams, MagicLinkParams};
 use shared_entity::dto::workspace_dto::{
   CreateWorkspaceMember, WorkspaceMemberChangeset, WorkspaceMemberInvitation,
 };
@@ -164,7 +164,6 @@ pub async fn accept_workspace_invite(
 #[instrument(level = "debug", skip_all, err)]
 pub async fn invite_workspace_members(
   pg_pool: &PgPool,
-  gotrue_admin: &GoTrueAdmin,
   gotrue_client: &gotrue::api::Client,
   inviter: &Uuid,
   workspace_id: &Uuid,
@@ -175,36 +174,16 @@ pub async fn invite_workspace_members(
     .await
     .context("Begin transaction to invite workspace members")?;
 
-  let admin_token = gotrue_admin.token(gotrue_client).await?;
   for invitation in invitations {
-    match gotrue_client
-      .admin_invite_user(
-        &admin_token,
-        &InviteUserParams {
+    gotrue_client
+      .magic_link(
+        &MagicLinkParams {
           email: invitation.email.clone(),
           ..Default::default()
         },
+        Some("/web/home#invite".to_owned()),
       )
-      .await
-    {
-      Ok(new_user) => {
-        info!(
-          "Invited new user: {:?} to workspace: {:?}",
-          new_user, workspace_id
-        );
-      },
-      Err(err) => match err {
-        app_error::gotrue::GoTrueError::Internal(ref err_serde) => {
-          match (err_serde.code, err_serde.msg.as_str()) {
-            (422, "A user with this email address has already been registered") => {
-              info!("User already exists, skipping invite");
-            },
-            _ => return Err(AppError::Internal(err.into())),
-          }
-        },
-        _ => return Err(err.into()),
-      },
-    }
+      .await?;
 
     // Generate a link such that when clicked, the user is added to the workspace.
     insert_workspace_invitation(

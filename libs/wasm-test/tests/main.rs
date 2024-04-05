2565c33773a3ddf040129e26ee8f5d918c1c6c0a
nathan <nathan@appflowy.io>
2024-04-05 11:13:46 +0800

    chore: disable wasm user test

--- libs/wasm-test/tests/main.rs
+++ libs/wasm-test/tests/main.rs
@@ -5,5 +5,5 @@ wasm_bindgen_test_configure!(run_in_browser);
 #[cfg(target_arch = "wasm32")]
 mod conn_test;
 
-#[cfg(target_arch = "wasm32")]
-mod user_test;
+// #[cfg(target_arch = "wasm32")]
+// mod user_test;

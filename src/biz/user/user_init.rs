53c094487a4dc2a4f78126a05a00977e2490e8e7
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2024-04-06 21:04:12 +0800

    chore: create default workspace database (#454)
    * chore: create default workspace database
    
    * chore: fix clppy

--- src/biz/user/user_init.rs
+++ src/biz/user/user_init.rs
@@ -1,6 +1,12 @@
 use crate::biz::collab::storage::CollabAccessControlStorage;
 use app_error::AppError;
+
+use collab::core::origin::CollabOrigin;
+use collab::preclude::{Any, Collab, MapPrelim};
+use collab_entity::define::WORKSPACE_DATABASES;
+use collab_entity::CollabType;
 use database::collab::CollabStorage;
+use database::pg_row::AFWorkspaceRow;
 use database_entity::dto::CollabParams;
 use sqlx::Transaction;
 use std::sync::Arc;
@@ -12,7 +18,7 @@ use workspace_template::{WorkspaceTemplate, WorkspaceTemplateBuilder};
 #[instrument(level = "debug", skip_all, err)]
 pub async fn initialize_workspace_for_user<T>(
   uid: i64,
-  workspace_id: &str,
+  row: &AFWorkspaceRow,
   txn: &mut Transaction<'_, sqlx::Postgres>,
   templates: Vec<T>,
   collab_storage: &Arc<CollabAccessControlStorage>,
@@ -20,10 +26,29 @@ pub async fn initialize_workspace_for_user<T>(
 where
   T: WorkspaceTemplate + Send + Sync + 'static,
 {
-  let templates = WorkspaceTemplateBuilder::new(uid, workspace_id)
+  let workspace_id = row.workspace_id.to_string();
+  let templates = WorkspaceTemplateBuilder::new(uid, &workspace_id)
     .with_templates(templates)
     .build()
     .await?;
+
+  // Create a workspace database object for given user
+  // The database_storage_id is auto-generated when the workspace is created. So, it should be available
+  if let Some(database_storage_id) = row.database_storage_id.as_ref() {
+    let workspace_database_object_id = database_storage_id.to_string();
+    create_workspace_database_collab(
+      &workspace_id,
+      &uid,
+      &workspace_database_object_id,
+      collab_storage,
+      txn,
+    )
+    .await?;
+  } else {
+    return Err(AppError::Internal(anyhow::anyhow!(
+      "Workspace database object id is missing"
+    )));
+  }
 
   debug!("create {} templates for user:{}", templates.len(), uid);
   for template in templates {
@@ -35,7 +60,7 @@ where
 
     collab_storage
       .insert_or_update_collab_with_transaction(
-        workspace_id,
+        &workspace_id,
         &uid,
         CollabParams {
           object_id,
@@ -46,5 +71,43 @@ where
       )
       .await?;
   }
+  Ok(())
+}
+
+async fn create_workspace_database_collab(
+  workspace_id: &str,
+  uid: &i64,
+  object_id: &str,
+  storage: &Arc<CollabAccessControlStorage>,
+  txn: &mut Transaction<'_, sqlx::Postgres>,
+) -> Result<(), AppError> {
+  let collab_type = CollabType::WorkspaceDatabase;
+  let collab = Collab::new_with_origin(CollabOrigin::Empty, object_id, vec![], false);
+  let _ = collab.with_origin_transact_mut(|txn| {
+    collab.create_array_with_txn::<MapPrelim<Any>>(txn, WORKSPACE_DATABASES, vec![]);
+    Ok::<(), AppError>(())
+  });
+
+  let encode_collab = collab
+    .encode_collab_v1(|collab| collab_type.validate(collab))
+    .map_err(AppError::Internal)?;
+
+  let encoded_collab_v1 = encode_collab
+    .encode_to_bytes()
+    .map_err(|err| AppError::Internal(anyhow::Error::from(err)))?;
+
+  storage
+    .insert_or_update_collab_with_transaction(
+      workspace_id,
+      uid,
+      CollabParams {
+        object_id: object_id.to_string(),
+        encoded_collab_v1,
+        collab_type,
+      },
+      txn,
+    )
+    .await?;
+
   Ok(())
 }

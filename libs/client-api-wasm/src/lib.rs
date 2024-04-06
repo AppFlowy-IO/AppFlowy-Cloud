179f11ce4045d215fe2216814581acbd31a8127c
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2024-04-06 16:43:38 +0800

    feat: add feature brotli (#452)
    * feat: add feature brotli
    
    * chore: update reqwest version
    
    * chore: update actix-http
    
    * chore: fix wasm build

--- libs/client-api-wasm/src/lib.rs
+++ libs/client-api-wasm/src/lib.rs
@@ -3,9 +3,7 @@ use crate::entities::{ClientAPIConfig, ClientResponse};
 use client_api::{Client, ClientConfiguration};
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
+#[cfg(feature = "enable_wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 

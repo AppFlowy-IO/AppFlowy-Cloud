196d200a00b81cc79d108979c25bb45f3407cc52
nathan <nathan@appflowy.io>
2024-04-08 14:50:16 +0800

    chore: add test

--- libs/collab-rt/src/group/plugin/history_plugin.rs
+++ libs/collab-rt/src/group/plugin/history_plugin.rs
@@ -59,7 +59,7 @@ where
     }
 
     let old = self.edit_count.fetch_add(1, Ordering::SeqCst);
-    if old < 10 {
+    if old < 20 {
       trace!("skip snapshot creation, edit_count: {}", old);
       return;
     }

90e9df87cef6fda0e03d3b1c26be0598c7876ed0
nathan <nathan@appflowy.io>
2024-04-16 13:43:22 +0800

    chore: fix ci

--- services/appflowy-history/src/main.rs
+++ services/appflowy-history/src/main.rs
@@ -5,9 +5,7 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let listener = TcpListener::bind("0.0.0.0:50051")
-    .await
-    .expect("failed to bind to port: 50051");
+  let listener = TcpListener::bind("0.0.0.0:50051").await.unwrap();
   let config = Config::from_env().expect("failed to load config");
   run_server(listener, config).await
 }

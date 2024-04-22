ca8dcdfd556495d13bd79683c9406e4d839ab464
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-04-22 11:28:30 +0800

    fix: error message remove code

--- libs/app-error/src/lib.rs
+++ libs/app-error/src/lib.rs
@@ -223,7 +223,7 @@ impl From<crate::gotrue::GoTrueError> for AppError {
         },
         (401, _) => AppError::UserUnAuthorized(format!("{}:{}", err.code, err.msg)),
         (422, _) => AppError::InvalidRequest(err.msg),
-        _ => AppError::OAuthError(err.to_string()),
+        _ => AppError::OAuthError(err.msg),
       },
       GoTrueError::Unhandled(err) => AppError::Internal(err),
       GoTrueError::NotLoggedIn(msg) => AppError::NotLoggedIn(msg),

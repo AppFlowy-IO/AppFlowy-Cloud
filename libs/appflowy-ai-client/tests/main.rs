d46ff73433c1880346535eb1eae778c10a3680f2
nathan <nathan@appflowy.io>
2024-05-12 17:43:48 +0800

    chore: fix ci

--- libs/appflowy-ai-client/tests/main.rs
+++ libs/appflowy-ai-client/tests/main.rs
@@ -6,7 +6,5 @@ mod row_test;
 // mod index_test;
 
 pub fn appflowy_ai_client() -> AppFlowyAIClient {
-  let url =
-    std::env::var("APPFLOWY_AI_URI").unwrap_or_else(|_| "http://localhost:5001".to_string());
-  AppFlowyAIClient::new(&url)
+  AppFlowyAIClient::new("http://localhost:5001")
 }

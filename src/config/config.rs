4e2da2d9092ec451b4f36641a051d36a586928f7
Zack <speed2exe@live.com.sg>
2024-05-18 08:33:08 +0800

    chore: Self host improvement (#561)
    * fix: remove multiple ways to specify database name
    
    * feat: resent email for already invited user
    
    * feat: mailer address from smtp username
    
    * feat: allow user defined smtp port

--- src/config/config.rs
+++ src/config/config.rs
@@ -23,6 +23,7 @@ pub struct Config {
 #[derive(serde::Deserialize, Clone, Debug)]
 pub struct MailerSetting {
   pub smtp_host: String,
+  pub smtp_port: u16,
   pub smtp_username: String,
   pub smtp_password: Secret<String>,
 }
@@ -92,21 +93,20 @@ pub struct DatabaseSetting {
   /// connections are reserved for system applications.
   /// When we exceed the limit of the database connection, then it shows an error message.
   pub max_connections: u32,
-  pub database_name: String,
 }
 
 impl Display for DatabaseSetting {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(
-        f,
-        "DatabaseSetting {{ pg_conn_opts: {:?}, require_ssl: {}, max_connections: {}, database_name: {} }}",
-        self.pg_conn_opts, self.require_ssl, self.max_connections, self.database_name
-        )
+      f,
+      "DatabaseSetting {{ pg_conn_opts: {:?}, require_ssl: {}, max_connections: {} }}",
+      self.pg_conn_opts, self.require_ssl, self.max_connections
+    )
   }
 }
 
 impl DatabaseSetting {
-  pub fn without_db(&self) -> PgConnectOptions {
+  pub fn pg_connect_options(&self) -> PgConnectOptions {
     let ssl_mode = if self.require_ssl {
       PgSslMode::Require
     } else {
@@ -114,10 +114,6 @@ impl DatabaseSetting {
     };
     let options = self.pg_conn_opts.clone();
     options.ssl_mode(ssl_mode)
-  }
-
-  pub fn with_db(&self) -> PgConnectOptions {
-    self.without_db().database(&self.database_name)
   }
 }
 
@@ -143,7 +139,6 @@ pub fn get_configuration() -> Result<Config, anyhow::Error> {
       max_connections: get_env_var("APPFLOWY_DATABASE_MAX_CONNECTIONS", "40")
         .parse()
         .context("fail to get APPFLOWY_DATABASE_MAX_CONNECTIONS")?,
-      database_name: get_env_var("APPFLOWY_DATABASE_NAME", "postgres"),
     },
     gotrue: GoTrueSetting {
       base_url: get_env_var("APPFLOWY_GOTRUE_BASE_URL", "http://localhost:9999"),
@@ -184,6 +179,7 @@ pub fn get_configuration() -> Result<Config, anyhow::Error> {
     },
     mailer: MailerSetting {
       smtp_host: get_env_var("APPFLOWY_MAILER_SMTP_HOST", "smtp.gmail.com"),
+      smtp_port: get_env_var("APPFLOWY_MAILER_SMTP_PORT", "465").parse()?,
       smtp_username: get_env_var("APPFLOWY_MAILER_SMTP_USERNAME", "sender@example.com"),
       smtp_password: get_env_var("APPFLOWY_MAILER_SMTP_PASSWORD", "password").into(),
     },

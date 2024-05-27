0a3b21b05791345946fedba3b9534c0bfa9c7dc5
nathan <nathan@appflowy.io>
2024-05-27 10:14:43 +0800

    chore: remove ai nginx config

--- tests/main.rs
+++ tests/main.rs
@@ -1,10 +1,10 @@
+#[cfg(feature = "ai_enabled")]
+mod ai_test;
 mod collab;
+mod collab_history;
 mod gotrue;
 mod sql_test;
 mod user;
 mod websocket;
 mod workspace;
-
-mod ai_test;
-mod collab_history;
 mod yrs_version;

73508664357bb6685f23d9ad7ade0f1936570a09
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2024-05-30 07:50:25 +0800

    chore: fix ai client test (#589)

--- libs/appflowy-ai-client/tests/chat_test/qa_test.rs
+++ libs/appflowy-ai-client/tests/chat_test/qa_test.rs
@@ -4,8 +4,9 @@ use crate::appflowy_ai_client;
 async fn qa_test() {
   let client = appflowy_ai_client();
   client.health_check().await.unwrap();
+  let chat_id = uuid::Uuid::new_v4().to_string();
   let resp = client
-    .send_question("fake_chat_id", "I feel hungry")
+    .send_question(&chat_id, "I feel hungry")
     .await
     .unwrap();
   assert!(!resp.content.is_empty());

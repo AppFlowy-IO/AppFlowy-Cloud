128e97801674b8d4291481741e42358aa8509b93
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2024-05-29 17:45:30 +0200

    chore: add appflowy indexer service start to cargo run xtask (#586)

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -5,6 +5,7 @@ use tokio::select;
 /// Using 'cargo run --package xtask' to run servers in parallel.
 /// 1. AppFlowy Cloud
 /// 2. AppFlowy History
+/// 3. AppFlowy Indexer
 ///
 /// Before running this command, make sure the other dependencies servers are running. For example,
 /// Redis, Postgres, etc.
@@ -12,9 +13,11 @@ use tokio::select;
 async fn main() -> Result<()> {
   let appflowy_cloud_bin_name = "appflowy_cloud";
   let appflowy_history_bin_name = "appflowy_history";
+  let appflowy_indexer_bin_name = "appflowy_indexer";
 
   kill_existing_process(appflowy_cloud_bin_name).await?;
   kill_existing_process(appflowy_history_bin_name).await?;
+  kill_existing_process(appflowy_indexer_bin_name).await?;
 
   let mut appflowy_cloud_cmd = Command::new("cargo")
     .args(["run", "--features", "history"])
@@ -30,12 +33,24 @@ async fn main() -> Result<()> {
     .spawn()
     .context("Failed to start AppFlowy-History process")?;
 
+  let mut appflowy_indexer_cmd = Command::new("cargo")
+    .args([
+      "run",
+      "--manifest-path",
+      "./services/appflowy-indexer/Cargo.toml",
+    ])
+    .spawn()
+    .context("Failed to start AppFlowy-Indexer process")?;
+
   select! {
       status = appflowy_cloud_cmd.wait() => {
           handle_process_exit(status?, appflowy_cloud_bin_name)?;
       },
       status = appflowy_history_cmd.wait() => {
           handle_process_exit(status?, appflowy_history_bin_name)?;
+      },
+      status = appflowy_indexer_cmd.wait() => {
+          handle_process_exit(status?, appflowy_indexer_bin_name)?;
       },
   }
 

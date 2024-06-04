85967b11027b35fe71c925a989a3861b6f4ddf5c
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2024-06-04 10:23:19 +0200

    chore: silence flaky test

--- services/appflowy-indexer/src/main.rs
+++ services/appflowy-indexer/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::sync::{Arc, Once};
+use std::sync::Arc;
 use tracing::subscriber::set_global_default;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::EnvFilter;
@@ -76,36 +76,33 @@ async fn run_server(config: Config) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn init_subscriber(app_env: &Environment) {
-  static START: Once = Once::new();
-  START.call_once(|| {
-    let level = std::env::var("RUST_LOG").unwrap_or("info".to_string());
-    let mut filters = vec![];
-    filters.push(format!("appflowy_history={}", level));
-    let env_filter = EnvFilter::new(filters.join(","));
+  let level = std::env::var("RUST_LOG").unwrap_or("info".to_string());
+  let mut filters = vec![];
+  filters.push(format!("appflowy_indexer={}", level));
+  let env_filter = EnvFilter::new(filters.join(","));
 
-    let builder = tracing_subscriber::fmt()
-      .with_target(true)
-      .with_max_level(tracing::Level::TRACE)
-      .with_thread_ids(false)
-      .with_file(false);
+  let builder = tracing_subscriber::fmt()
+    .with_target(true)
+    .with_max_level(tracing::Level::TRACE)
+    .with_thread_ids(false)
+    .with_file(false);
 
-    match app_env {
-      Environment::Local => {
-        let subscriber = builder
-          .with_ansi(true)
-          .with_target(false)
-          .with_file(false)
-          .pretty()
-          .finish()
-          .with(env_filter);
-        set_global_default(subscriber).unwrap();
-      },
-      Environment::Production => {
-        let subscriber = builder.json().finish().with(env_filter);
-        set_global_default(subscriber).unwrap();
-      },
-    }
-  });
+  match app_env {
+    Environment::Local => {
+      let subscriber = builder
+        .with_ansi(true)
+        .with_target(false)
+        .with_file(false)
+        .pretty()
+        .finish()
+        .with(env_filter);
+      set_global_default(subscriber).unwrap();
+    },
+    Environment::Production => {
+      let subscriber = builder.json().finish().with(env_filter);
+      set_global_default(subscriber).unwrap();
+    },
+  }
 }
 
 #[derive(Clone, Debug, clap::ValueEnum)]

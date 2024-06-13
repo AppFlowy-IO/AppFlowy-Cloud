b0a48494e58cdd4e9ff9b48e190c7d93d071af0b
nathan <nathan@appflowy.io>
2024-06-13 21:35:50 +0800

    chore: fix chat stream

--- libs/client-api/src/http_chat.rs
+++ libs/client-api/src/http_chat.rs
@@ -1,5 +1,6 @@
 use crate::http::log_request_id;
 use crate::Client;
+use bytes::Bytes;
 use database_entity::dto::{
   ChatMessage, CreateAnswerMessageParams, CreateChatMessageParams, CreateChatParams, MessageCursor,
   RepeatedChatMessage, UpdateChatMessageContentParams,
@@ -109,7 +110,7 @@ impl Client {
     workspace_id: &str,
     chat_id: &str,
     message_id: i64,
-  ) -> Result<impl Stream<Item = Result<StringOrMessage, AppResponseError>>, AppResponseError> {
+  ) -> Result<impl Stream<Item = Result<Bytes, AppResponseError>>, AppResponseError> {
     let url = format!(
       "{}/api/chat/{workspace_id}/{chat_id}/{message_id}/answer/stream",
       self.base_url

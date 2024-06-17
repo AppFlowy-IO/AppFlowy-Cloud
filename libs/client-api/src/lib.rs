2a71ddae7e82c37fe0f6eec713c72cf74db9a59e
nathan <nathan@appflowy.io>
2024-06-17 10:36:25 +0800

    chore: expose file entity

--- libs/client-api/src/lib.rs
+++ libs/client-api/src/lib.rs
@@ -38,6 +38,7 @@ pub mod error {
 pub mod entity {
   pub use collab_rt_entity::user::*;
   pub use database_entity::dto::*;
+  pub use database_entity::file_dto::*;
   pub use gotrue_entity::dto::*;
   pub use shared_entity::dto::*;
 }

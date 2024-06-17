b911ce37699776e44b9f415e2848aed8a2974f39
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-06-17 14:33:14 +0800

    chore: remove unneeded key

--- migrations/20240617135926_af_workspace_foreign_key_indices.sql
+++ migrations/20240617135926_af_workspace_foreign_key_indices.sql
@@ -5,4 +5,3 @@ CREATE INDEX IF NOT EXISTS idx_workspace_id_on_af_collab                 ON af_c
 CREATE INDEX IF NOT EXISTS idx_workspace_id_on_af_snapshot_meta          ON af_snapshot_meta          (workspace_id);
 CREATE INDEX IF NOT EXISTS idx_workspace_id_on_af_snapshot_state         ON af_snapshot_state         (workspace_id);
 CREATE INDEX IF NOT EXISTS idx_workspace_id_on_af_workspace_member       ON af_workspace_member       (workspace_id);
-CREATE INDEX IF NOT EXISTS idx_workspace_id_on_af_workspace_usage_limits ON af_workspace_usage_limits (workspace_id);

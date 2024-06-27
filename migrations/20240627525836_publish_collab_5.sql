d870cd6897141b9df1ed57d91b1b159c6e7bad26
Zack <speed2exe@live.com.sg>
2024-06-27 19:03:29 +0800

    fix: sqlx comments

--- migrations/20240627525836_publish_collab_5.sql
+++ migrations/20240627525836_publish_collab_5.sql
@@ -2,6 +2,6 @@
 ALTER TABLE public.af_published_collab
 DROP CONSTRAINT unique_publish_name;
 
--- Add a new unique constraint for the combination of publish_name and view_id
+-- Add a new unique constraint for the combination of publish_name and workspace_id
 ALTER TABLE public.af_published_collab
 ADD CONSTRAINT unique_workspace_id_publish_name UNIQUE (workspace_id, publish_name);

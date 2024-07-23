47f87cee1c552f5893ea0458d8d29c50bdc3aecd
Kilu.He <108015703+qinluhe@users.noreply.github.com>
2024-07-23 17:47:21 +0800

    feat: support publish interfaces for wasm (#654)
    * feat: support publish interfaces for wasm
    
    * fix: fmt
    
    * feat: support sign in with url

--- libs/client-api-wasm/src/lib.rs
+++ libs/client-api-wasm/src/lib.rs
@@ -5,11 +5,13 @@ use crate::entities::*;
 use client_api::notify::TokenState;
 use client_api::{Client, ClientConfiguration};
 use std::sync::Arc;
+use uuid::Uuid;
 
+use client_api::error::ErrorCode;
+use console_error_panic_hook;
 use database_entity::dto::QueryCollab;
 use wasm_bindgen::prelude::*;
 
-#[cfg(feature = "enable_wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
@@ -49,6 +51,7 @@ pub struct ClientAPI {
 impl ClientAPI {
   pub fn new(config: ClientAPIConfig) -> ClientAPI {
     tracing_wasm::set_as_global_default();
+    console_error_panic_hook::set_once();
     let configuration = ClientConfiguration::default();
 
     if let Some(compression) = &config.configuration {
@@ -115,6 +118,52 @@ impl ClientAPI {
     }
   }
 
+  pub async fn sign_in_with_magic_link(
+    &self,
+    email: &str,
+    redirect_to: &str,
+  ) -> Result<(), ClientResponse> {
+    match self
+      .client
+      .sign_in_with_magic_link(email, Some(redirect_to.to_string()))
+      .await
+    {
+      Ok(_) => Ok(()),
+      Err(err) => Err(ClientResponse::from(err)),
+    }
+  }
+
+  pub async fn generate_oauth_url_with_provider(
+    &self,
+    provider: &str,
+    redirect_to: &str,
+  ) -> Result<OAuthURLResponse, ClientResponse> {
+    if provider.is_empty() {
+      return Err(ClientResponse {
+        code: ErrorCode::OAuthError,
+        message: "Provider is empty".to_string(),
+      });
+    }
+
+    let provider = parse_provider(provider);
+
+    match self
+      .client
+      .generate_url_with_provider_and_redirect_to(&provider, Some(redirect_to.to_string()))
+      .await
+    {
+      Ok(url) => Ok(OAuthURLResponse { url }),
+      Err(err) => Err(ClientResponse::from(err)),
+    }
+  }
+
+  pub async fn sign_in_with_url(&self, url: &str) -> Result<(), ClientResponse> {
+    match self.client.sign_in_with_url(url).await {
+      Ok(_) => Ok(()),
+      Err(err) => Err(ClientResponse::from(err)),
+    }
+  }
+
   pub async fn get_user(&self) -> Result<User, ClientResponse> {
     match self.client.get_profile().await {
       Ok(profile) => Ok(User::from(profile)),
@@ -157,6 +206,59 @@ impl ClientAPI {
   pub async fn get_user_workspace(&self) -> Result<UserWorkspace, ClientResponse> {
     match self.client.get_user_workspace_info().await {
       Ok(workspace_info) => Ok(UserWorkspace::from(workspace_info)),
+      Err(err) => Err(ClientResponse::from(err)),
+    }
+  }
+
+  pub async fn get_publish_view_meta(
+    &self,
+    publish_namespace: String,
+    publish_name: String,
+  ) -> Result<PublishViewMeta, ClientResponse> {
+    match self
+      .client
+      .get_published_collab::<serde_json::Value>(publish_namespace.as_str(), publish_name.as_str())
+      .await
+    {
+      Ok(data) => Ok(PublishViewMeta {
+        data: data.to_string(),
+      }),
+
+      Err(err) => Err(ClientResponse::from(err)),
+    }
+  }
+
+  pub async fn get_publish_view(
+    &self,
+    publish_namespace: String,
+    publish_name: String,
+  ) -> Result<PublishViewPayload, ClientResponse> {
+    let meta = self
+      .get_publish_view_meta(publish_namespace.clone(), publish_name.clone())
+      .await?;
+
+    let data = self
+      .client
+      .get_published_collab_blob(publish_namespace.as_str(), publish_name.as_str())
+      .await
+      .map_err(ClientResponse::from)?;
+
+    Ok(PublishViewPayload {
+      meta,
+      data: data.to_vec(),
+    })
+  }
+
+  pub async fn get_publish_info(&self, view_id: String) -> Result<PublishInfo, ClientResponse> {
+    let view_id = Uuid::parse_str(view_id.as_str()).map_err(|err| ClientResponse {
+      code: ErrorCode::UuidError,
+      message: format!("Failed to parse view_id: {}", err),
+    })?;
+    match self.client.get_published_collab_info(&view_id).await {
+      Ok(info) => Ok(PublishInfo {
+        namespace: info.namespace,
+        publish_name: info.publish_name,
+      }),
       Err(err) => Err(ClientResponse::from(err)),
     }
   }

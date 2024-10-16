c8529df838e3c26578306cdddbbfd2e25f63f821
khorshuheng <solemnpriest@gmail.com>
2024-10-16 21:27:03 +0800

    chore: test retroactive migration fix

--- migrations/20230906102652_collab.sql
+++ migrations/20230906102652_collab.sql
@@ -11,20 +11,20 @@ CREATE TABLE IF NOT EXISTS af_collab (
     workspace_id UUID NOT NULL REFERENCES af_workspace(workspace_id) ON DELETE CASCADE,
     PRIMARY KEY (oid, partition_key)
 ) PARTITION BY LIST (partition_key);
-CREATE TABLE af_collab_document PARTITION OF af_collab FOR
+CREATE TABLE IF NOT EXISTS af_collab_document PARTITION OF af_collab FOR
 VALUES IN (0);
-CREATE TABLE af_collab_database PARTITION OF af_collab FOR
+CREATE TABLE IF NOT EXISTS af_collab_database PARTITION OF af_collab FOR
 VALUES IN (1);
-CREATE TABLE af_collab_w_database PARTITION OF af_collab FOR
+CREATE TABLE IF NOT EXISTS af_collab_w_database PARTITION OF af_collab FOR
 VALUES IN (2);
-CREATE TABLE af_collab_folder PARTITION OF af_collab FOR
+CREATE TABLE IF NOT EXISTS af_collab_folder PARTITION OF af_collab FOR
 VALUES IN (3);
-CREATE TABLE af_collab_database_row PARTITION OF af_collab FOR
+CREATE TABLE IF NOT EXISTS af_collab_database_row PARTITION OF af_collab FOR
 VALUES IN (4);
-CREATE TABLE af_collab_user_awareness PARTITION OF af_collab FOR
+CREATE TABLE IF NOT EXISTS af_collab_user_awareness PARTITION OF af_collab FOR
 VALUES IN (5);
 
-CREATE TABLE af_collab_member (
+CREATE TABLE IF NOT EXISTS af_collab_member (
     uid BIGINT REFERENCES af_user(uid) ON DELETE CASCADE,
     oid TEXT NOT NULL,
     permission_id INTEGER REFERENCES af_permissions(id) NOT NULL,
@@ -32,8 +32,6 @@ CREATE TABLE af_collab_member (
 );
 
 -- Listener
-DROP TRIGGER IF EXISTS af_collab_member_change_trigger ON af_collab_member;
-
 CREATE OR REPLACE FUNCTION notify_af_collab_member_change() RETURNS trigger AS $$
 DECLARE
 payload TEXT;
@@ -54,7 +52,7 @@ END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER af_collab_member_change_trigger
+CREATE OR REPLACE TRIGGER af_collab_member_change_trigger
     AFTER INSERT OR UPDATE OR DELETE ON af_collab_member
     FOR EACH ROW EXECUTE FUNCTION notify_af_collab_member_change();
 
@@ -69,9 +67,4 @@ CREATE TABLE IF NOT EXISTS af_collab_snapshot (
     workspace_id UUID NOT NULL REFERENCES af_workspace(workspace_id) ON DELETE CASCADE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
-CREATE INDEX idx_af_collab_snapshot_oid ON af_collab_snapshot(oid);
--- Enable RLS on the af_collab table
-ALTER TABLE af_collab ENABLE ROW LEVEL SECURITY;
-CREATE POLICY af_collab_policy ON af_collab FOR ALL TO anon,
-    authenticated USING (true);
-ALTER TABLE af_collab FORCE ROW LEVEL SECURITY;
+CREATE INDEX IF NOT EXISTS idx_af_collab_snapshot_oid ON af_collab_snapshot(oid);

167d8fd7da6b8793e29165d330b3c49135e04098
nathan <nathan@appflowy.io>
2024-10-17 12:08:03 +0800

    chore: remove workspace and collab listener

--- src/biz/pg_listener.rs
+++ src/biz/pg_listener.rs
@@ -4,39 +4,15 @@ use appflowy_collaborate::collab::notification::CollabMemberNotification;
 use database::listener::PostgresDBListener;
 use database::pg_row::AFUserNotification;
 use sqlx::PgPool;
-use tokio::sync::broadcast;
 
 pub struct PgListeners {
   user_listener: UserListener,
-  workspace_member_listener: WorkspaceMemberListener,
-  collab_member_listener: CollabMemberListener,
 }
 
 impl PgListeners {
   pub async fn new(pg_pool: &PgPool) -> Result<Self, Error> {
     let user_listener = UserListener::new(pg_pool, "af_user_channel").await?;
-
-    let workspace_member_listener =
-      WorkspaceMemberListener::new(pg_pool, "af_workspace_member_channel").await?;
-
-    let collab_member_listener =
-      CollabMemberListener::new(pg_pool, "af_collab_member_channel").await?;
-
-    Ok(Self {
-      user_listener,
-      workspace_member_listener,
-      collab_member_listener,
-    })
-  }
-
-  pub fn subscribe_workspace_member_change(
-    &self,
-  ) -> broadcast::Receiver<WorkspaceMemberNotification> {
-    self.workspace_member_listener.notify.subscribe()
-  }
-
-  pub fn subscribe_collab_member_change(&self) -> broadcast::Receiver<CollabMemberNotification> {
-    self.collab_member_listener.notify.subscribe()
+    Ok(Self { user_listener })
   }
 
   pub fn subscribe_user_change(&self, uid: i64) -> tokio::sync::mpsc::Receiver<AFUserNotification> {

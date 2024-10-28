c805f12421594652adbb6f5f8c2008f9d47c80ef
nathan <nathan@appflowy.io>
2024-10-28 13:58:39 +0800

    chore: fix import test

--- script/run_ci_server.sh
+++ script/run_ci_server.sh
@@ -4,30 +4,53 @@ set -eo pipefail
 
 cd "$(dirname "$0")/.."
 
-# This script simulates the continuous integration (CI) environment on a local machine. It
-# requires a `.env` file to be located in the project's root directory. The values in the `.env`
-# file must be updated to reflect the specifications of the CI environment.
 # Check if .env file exists in the current directory
-
 if [ -f ".env" ]; then
   echo ".env file exists"
 else
-  echo ".env file does not exist. Copying deploy.env to .env and update the values"
+  echo ".env file does not exist. Please copy deploy.env to .env and update the values."
   exit 1  # Exit with an error code to indicate failure
 fi
 
-# Make sure to update the test client configuration in libs/client-api-test-util/src/client.rs
-# export LOCALHOST_URL="http://localhost"
-# export LOCALHOST_WS_URL="ws://localhost/ws/v1"
-# export LOCALHOST_GOTRUE_URL="http://localhost:gotrue"
+IMAGE_VERSION="${1:-latest}"  # Default to 'latest' if no argument is provided
 
+
+# Stop and remove running containers
 docker compose down
-docker compose -f docker-compose-ci.yml pull
 
-# SKIP_BUILD_APPFLOWY_CLOUD=true.
-if [[ -z "${SKIP_BUILD_APPFLOWY_CLOUD+x}" ]]
-then
-  docker build -t appflowy_cloud . && docker build -t appflowy_worker .
+# Build amd64 images with a new local tag
+# For example: SKIP_BUILD=true  ./script/run_ci_server.sh 0.6.51-amd64
+if [[ -z "${SKIP_BUILD+x}" ]]; then
+  docker build --platform=linux/amd64 -t appflowyinc/appflowy_cloud_local:$IMAGE_VERSION -f Dockerfile .
+  docker build --platform=linux/amd64 -t appflowyinc/appflowy_worker_local:$IMAGE_VERSION -f ./services/appflowy-worker/Dockerfile .
+  
+  cat > docker-compose.override.yml <<EOF
+version: '3'
+services:
+  appflowy_cloud:
+    image: appflowyinc/appflowy_cloud:$IMAGE_VERSION
+  appflowy_worker:
+    image: appflowyinc/appflowy_worker:$IMAGE_VERSION
+EOF
+
+
+  export RUST_LOG=trace
+  docker compose -f docker-compose-ci.yml -f docker-compose.override.yml up -d --build
+  rm docker-compose.override.yml
+else
+  echo "Skipping the build process for appflowy services..."
+  echo "Using image version: $IMAGE_VERSION"
+
+  # Set the image version to the input value
+  export RUST_LOG=trace
+  export APPFLOWY_CLOUD_VERSION=$IMAGE_VERSION
+  export APPFLOWY_WORKER_VERSION=$IMAGE_VERSION
+  export APPFLOWY_HISTORY_VERSION=$IMAGE_VERSION
+  export APPFLOWY_ADMIN_FRONTEND_VERSION=$IMAGE_VERSION
+  docker compose -f docker-compose-ci.yml pull
+
+  echo "Printing the appflowy_cloud image version:"
+  docker images appflowyinc/appflowy_cloud --format "{{.Repository}}:{{.Tag}} (Created: {{.CreatedSince}}, Size: {{.Size}})"
+
+  docker compose -f docker-compose-ci.yml up -d
 fi
-
-docker compose -f docker-compose-ci.yml up -d

254bc23a6e79bf9f80da7cd14a1ce48750b49ea3
Zack <speed2exe@live.com.sg>
2024-12-11 15:47:29 +0800

    feat: insert database row into selected database (#1029)
    * feat: insert database row into selected database
    
    * feat: inserting database row
    
    * feat: add impl for cell
    
    * feat: insert row selection and mulitselection
    
    * feat: support datetime field type
    
    * fix: other fields like rich text
    
    * feat: add database field server impl
    
    * feat: add client api and tests
    
    * feat: use to json value impl from collab
    
    * feat: use add database cell impl from collab
    
    * feat: update to latest collab
    
    * chore: upgrade collab and fix tests
    
    * chore: review issues
    
    * fix: code review feedback
    
    * feat: filter only allow supported field types
    
    * feat: support more field types
    
    * feat: support created at and last modified
    
    * feat: add timestamp cell for created at and modified at fields
    
    * chore: linting and formatting
    
    * fix: add created at and last modified
    
    * fix: ci add service dependency of appflowy cloud on admin frontend
    
    * chore: trigger ci
    
    * feat: add logging for admin frontend client signin
    
    * fix: server logs if error
    
    * fix: create admin confirmation without email
    
    * fix: ci add service dependency of appflowy cloud on admin frontend
    
    * fix: server logs if error
    
    * chore: update collab

--- services/appflowy-collaborate/src/collab/cache/mem_cache.rs
+++ services/appflowy-collaborate/src/collab/cache/mem_cache.rs
@@ -32,7 +32,7 @@ impl CollabMemCache {
   pub async fn insert_collab_meta(&self, meta: CollabMetadata) -> Result<(), AppError> {
     let key = collab_meta_key(&meta.object_id);
     let value = serde_json::to_string(&meta)?;
-    self
+    let () = self
       .connection_manager
       .clone()
       .set_ex(key, value, ONE_MONTH)
@@ -188,7 +188,7 @@ impl CollabMemCache {
       // for executing a subsequent transaction (with MULTI/EXEC). If any of the watched keys are
       // altered by another client before the current client executes EXEC, the transaction will be
       // aborted by Redis (the EXEC will return nil indicating the transaction was not processed).
-      redis::cmd("WATCH")
+      let () = redis::cmd("WATCH")
         .arg(&cache_object_id)
         .query_async::<_, ()>(&mut conn)
         .await?;
@@ -228,7 +228,7 @@ impl CollabMemCache {
             .ignore()
             .expire(&cache_object_id, expiration_seconds.unwrap_or(SEVEN_DAYS) as i64) // Setting the expiration to 7 days
             .ignore();
-        pipeline.query_async(&mut conn).await?;
+        let () = pipeline.query_async(&mut conn).await?;
       }
       Ok::<(), redis::RedisError>(())
     }

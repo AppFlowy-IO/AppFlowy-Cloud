48a2e813b931db307414550b8c9688cdb5cbffbe
nathan <nathan@appflowy.io>
2024-12-24 15:42:52 +0800

    chore: fix test

--- libs/collab-stream/tests/collab_stream_test/stream_group_test.rs
+++ libs/collab-stream/tests/collab_stream_test/stream_group_test.rs
@@ -114,14 +114,23 @@ async fn different_group_read_undelivered_message_test() {
 async fn different_group_read_message_test() {
   let oid = format!("o{}", random_i64());
   let client = stream_client().await;
-  let mut group_1 = client.collab_update_stream("w1", &oid, "g1").await.unwrap();
-  let mut group_2 = client.collab_update_stream("w1", &oid, "g2").await.unwrap();
+  let mut group_1 = client
+    .collab_update_stream_group("w1", &oid, "g1")
+    .await
+    .unwrap();
+  let mut group_2 = client
+    .collab_update_stream_group("w1", &oid, "g2")
+    .await
+    .unwrap();
 
   let msg = StreamBinary(vec![1, 2, 3, 4, 5]);
 
   {
     let client = stream_client().await;
-    let mut group = client.collab_update_stream("w1", &oid, "g2").await.unwrap();
+    let mut group = client
+      .collab_update_stream_group("w1", &oid, "g2")
+      .await
+      .unwrap();
     group.insert_binary(msg).await.unwrap();
   }
   let msg = group_1

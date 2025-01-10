56ced870a2c18b1843a34606297f339ab59b66b4
Khor Shu Heng <32997938+khorshuheng@users.noreply.github.com>
2025-01-10 16:30:54 +0800

    feat: add appflowy web url to server info endpoint (#1148)

--- src/api/server_info.rs
+++ src/api/server_info.rs
@@ -1,17 +1,23 @@
+use actix_web::web::Data;
 use actix_web::{web, Scope};
 use shared_entity::dto::server_info_dto::ServerInfoResponseItem;
 use shared_entity::response::{AppResponse, JsonAppResponse};
+
+use crate::state::AppState;
 
 pub fn server_info_scope() -> Scope {
   web::scope("/api/server").service(web::resource("").route(web::get().to(server_info_handler)))
 }
 
-async fn server_info_handler() -> actix_web::Result<JsonAppResponse<ServerInfoResponseItem>> {
+async fn server_info_handler(
+  state: Data<AppState>,
+) -> actix_web::Result<JsonAppResponse<ServerInfoResponseItem>> {
   Ok(
     AppResponse::Ok()
       .with_data(ServerInfoResponseItem {
         supported_client_features: vec![],
         minimum_supported_client_version: None,
+        appflowy_web_url: state.config.appflowy_web_url.clone(),
       })
       .into(),
   )

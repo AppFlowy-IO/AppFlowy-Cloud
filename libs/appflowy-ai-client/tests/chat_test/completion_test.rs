e40b048f9f1ea3993da3246ff2f0ea7fb759be18
weidong fu <nathan@appflowy.io>
2025-01-11 23:11:49 +0800

    chore: update test

--- libs/appflowy-ai-client/tests/chat_test/completion_test.rs
+++ libs/appflowy-ai-client/tests/chat_test/completion_test.rs
@@ -6,12 +6,12 @@ async fn continue_writing_test() {
   let client = appflowy_ai_client();
   let params = CompleteTextParams {
     text: "I feel hungry".to_string(),
-    completion_type: Some(CompletionType::ContinueWriting),
+    completion_type: Some(CompletionType::ImproveWriting),
     custom_prompt: None,
     metadata: None,
   };
   let stream = client
-    .stream_completion_text(params, AIModel::Claude3Sonnet)
+    .stream_completion_text(params, AIModel::GPT4oMini)
     .await
     .unwrap();
   let text = collect_stream_text(stream).await;

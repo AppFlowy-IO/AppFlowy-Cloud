0d0547f440bc20a817f43d9606b359114087d7d9
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2025-01-20 23:22:33 +0800

    chore: add metrics for ai (#1184)

--- src/biz/chat/metrics.rs
+++ src/biz/chat/metrics.rs
@@ -5,6 +5,9 @@ pub struct AIMetrics {
   total_stream_count: Counter,
   failed_stream_count: Counter,
   stream_image_count: Counter,
+  total_completion_count: Counter,
+  total_summary_row_count: Counter,
+  total_translate_row_count: Counter,
 }
 
 impl AIMetrics {
@@ -28,6 +31,21 @@ impl AIMetrics {
       "Total count of image streams processed",
       metrics.stream_image_count.clone(),
     );
+    realtime_registry.register(
+      "total_completion_count",
+      "Total count of completions processed",
+      metrics.total_completion_count.clone(),
+    );
+    realtime_registry.register(
+      "total_summary_row_count",
+      "Total count of summary rows processed",
+      metrics.total_summary_row_count.clone(),
+    );
+    realtime_registry.register(
+      "total_translate_row_count",
+      "Total count of translation rows processed",
+      metrics.total_translate_row_count.clone(),
+    );
 
     metrics
   }
@@ -42,5 +60,17 @@ impl AIMetrics {
 
   pub fn record_stream_image_count(&self, count: u64) {
     self.stream_image_count.inc_by(count);
+  }
+
+  pub fn record_total_completion_count(&self, count: u64) {
+    self.total_completion_count.inc_by(count);
+  }
+
+  pub fn record_total_summary_row_count(&self, count: u64) {
+    self.total_summary_row_count.inc_by(count);
+  }
+
+  pub fn record_total_translate_row_count(&self, count: u64) {
+    self.total_translate_row_count.inc_by(count);
   }
 }

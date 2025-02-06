ee69a4e5b29f05dab5be0842538f6d9db70a51f0
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2025-02-06 12:56:15 +0800

    chore: support explain text (#1213)

--- tests/workspace/import_test.rs
+++ tests/workspace/import_test.rs
@@ -193,10 +193,6 @@ async fn upload_file(
     .await?
     .presigned_url;
 
-  // if url.contains("http://minio:9000") {
-  //   url = url.replace("http://minio:9000", "http://localhost/minio");
-  // }
-
   if let Some(secs) = upload_after_secs {
     tokio::time::sleep(Duration::from_secs(secs)).await;
   }
@@ -215,9 +211,9 @@ async fn import_notion_zip_until_complete(name: &str) -> (TestClient, String) {
   // Uncomment the following lines to use the predicated upload file API.
   // Currently, we use `upload_file` to send a file to appflowy_worker, which then
   // processes the upload task.
-  // let file_path = PathBuf::from(format!("tests/workspace/asset/{name}"));
-  // client.api_client.import_file(&file_path).await.unwrap();
-  upload_file(&client, name, None).await.unwrap();
+  let file_path = PathBuf::from(format!("tests/workspace/asset/{name}"));
+  client.api_client.import_file(&file_path).await.unwrap();
+  // upload_file(&client, name, None).await.unwrap();
 
   let default_workspace_id = client.workspace_id().await;
 

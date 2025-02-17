59786a686b0b221820f319eab8d65a76ea224aef
khorshuheng <solemnpriest@gmail.com>
2025-02-17 16:13:38 +0800

    chore: drop collab member trigger

---
+++ migrations/20250217080054_drop_collab_member_trigger.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION add_to_af_workspace_member()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.status = 1 THEN
+    -- workspace permission
+    INSERT INTO af_workspace_member (workspace_id, uid, role_id)
+    VALUES (
+      NEW.workspace_id,
+      (SELECT uid FROM af_user WHERE email = NEW.invitee_email),
+      NEW.role_id
+    )
+    ON CONFLICT (workspace_id, uid) DO NOTHING;
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS af_collab_member_change_trigger ON af_collab_member;
+DROP FUNCTION IF EXISTS notify_af_collab_member_change;

a2c10d60d642525d8ab9c58660b0423d59150ee3
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2025-03-18 11:30:57 +0800

    chore: support streaming v2 (#1279)

---
+++ tests/ai_test/completion_test.rs
@@ -0,0 +1,36 @@
+use appflowy_ai_client::dto::{CompleteTextParams, CompletionMetadata, CompletionType};
+use client_api_test::{ai_test_enabled, collect_completion_v2, TestClient};
+
+#[tokio::test]
+async fn generate_chat_message_answer_test() {
+  if !ai_test_enabled() {
+    return;
+  }
+  let test_client = TestClient::new_user_without_ws_conn().await;
+  let workspace_id = test_client.workspace_id().await;
+  let doc_id = uuid::Uuid::new_v4().to_string();
+
+  let params = CompleteTextParams {
+    text: "I seen the movie last night and it was amazing".to_string(),
+    completion_type: Some(CompletionType::SpellingAndGrammar),
+    custom_prompt: None,
+    metadata: Some(CompletionMetadata {
+      object_id: doc_id,
+      workspace_id: Some(workspace_id.clone()),
+      rag_ids: None,
+      completion_history: None,
+    }),
+    format: Default::default(),
+  };
+
+  let stream = test_client
+    .api_client
+    .stream_completion_v2(&workspace_id, params)
+    .await
+    .unwrap();
+  let (answer, comment) = collect_completion_v2(stream).await;
+  println!("answer: {}", answer);
+  println!("comment: {}", comment);
+  assert!(!answer.is_empty());
+  assert!(!comment.is_empty());
+}

2cbc23df7682d2403e14155d9674e8b69a56e97a
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2025-03-21 10:19:07 +0100

    chore: even more fixes

--- libs/collab-stream/src/awareness_gossip.rs
+++ libs/collab-stream/src/awareness_gossip.rs
@@ -131,10 +131,7 @@ mod test {
     for _ in 0..COLLAB_COUNT {
       let workspace_id = Uuid::new_v4();
       let object_id = Uuid::new_v4();
-      let sink = gossip
-        .sink(&workspace_id.to_string(), &object_id.to_string())
-        .await
-        .unwrap();
+      let sink = gossip.sink(&workspace_id, &object_id).await.unwrap();
       let stream = gossip.awareness_stream(&object_id);
       collabs.push((sink, stream));
     }

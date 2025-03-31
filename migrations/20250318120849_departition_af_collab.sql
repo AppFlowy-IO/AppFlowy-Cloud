ad0463a32dab40ed1732d3ce6a9dfbc87748bead
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2025-03-31 13:15:38 +0200

    chore: rollback from using foreign keys in embeddings

--- migrations/20250318120849_departition_af_collab.sql
+++ migrations/20250318120849_departition_af_collab.sql
@@ -1,3 +1,5 @@
+set statement_timeout to 3600000;
+
 -- create new uniform collab table
 create table af_collab_temp
 (
@@ -13,31 +15,44 @@ create table af_collab_temp
     indexed_at timestamp with time zone
 );
 
--- copy data from all collab partitions to new collab table
+-- create a table for collabs that have non-UUID object ids
+create table af_collab_conflicts
+(
+    oid text not null primary key,
+    workspace_id  uuid not null references public.af_workspace on delete cascade,
+    owner_uid bigint not null,
+    partition_key integer not null,
+    len integer,
+    blob bytea not null,
+    deleted_at timestamp with time zone,
+    created_at timestamp with time zone default CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone default CURRENT_TIMESTAMP not null,
+    indexed_at timestamp with time zone
+);
+
+-- move non-UUID object ids to the new table (execution time: 31 secs)
+insert into af_collab_conflicts(oid, workspace_id, owner_uid, partition_key, len, blob, deleted_at, created_at, updated_at, indexed_at)
+select oid, workspace_id, owner_uid, partition_key, len, blob, deleted_at, created_at, updated_at, indexed_at
+from af_collab
+where oid !~ E'^[[:xdigit:]]{8}-([[:xdigit:]]{4}-){3}[[:xdigit:]]{12}$';
+
+-- copy data from all collab partitions to new collab table (execution time: 7 min for 4mln rows)
 insert into af_collab_temp(oid, workspace_id, owner_uid, partition_key, len, blob, deleted_at, created_at, updated_at, indexed_at)
 select oid::uuid as oid, workspace_id, owner_uid, partition_key, len, blob, deleted_at, created_at, updated_at, indexed_at
-from af_collab;
+from af_collab
+where oid ~ E'^[[:xdigit:]]{8}-([[:xdigit:]]{4}-){3}[[:xdigit:]]{12}$';
 
 -- modify embeddings to make use of new uuid columns
 alter table af_collab_embeddings
-    add column object_id uuid null;
-update af_collab_embeddings set object_id = oid::uuid;
-alter table af_collab_embeddings
-    alter column object_id set not null;
-alter table af_collab_embeddings
-drop column oid,
-    drop column partition_key;
-alter table af_collab_embeddings rename column object_id to oid;
+drop constraint af_collab_embeddings_oid_partition_key_fkey;
+
+-- add index of oid (execution time: 5 secs)
+create index concurrently if not exists ix_af_collab_embeddings_oid
+    on af_collab_embeddings(oid);
 
 -- replace af_collab table
 drop table af_collab;
 alter table af_collab_temp rename to af_collab;
-
--- rebind embeddings foreign key to new af_collab table
-alter table af_collab_embeddings
-    add constraint fk_af_collab_embeddings_af_collab
-        foreign key (oid) references af_collab(oid);
-create index ix_af_collab_embeddings_oid on af_collab_embeddings(oid);
 
 -- add trigger for af_collab.updated_at
 create trigger set_updated_at
@@ -46,11 +61,10 @@ create trigger set_updated_at
     for each row
     execute procedure update_updated_at_column();
 
--- add remaining indexes to new af_collab table
-create index if not exists idx_workspace_id_on_af_collab
+-- add remaining indexes to new af_collab table (execution time: 25 sec + 25sec)
+create index concurrently if not exists idx_workspace_id_on_af_collab
     on af_collab (workspace_id);
-
-create index if not exists idx_af_collab_updated_at
+create index concurrently if not exists idx_af_collab_updated_at
     on af_collab (updated_at);
 
 create or replace procedure af_collab_embeddings_upsert(IN p_workspace_id uuid, IN p_oid uuid, IN p_tokens_used integer, IN p_fragments af_fragment_v3[])

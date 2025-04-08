6a9d502b50049dc45079580517fca56940d8d96f
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2025-04-08 12:31:27 +0200

    chore: test client v2

--- libs/client-api-test/src/lib.rs
+++ libs/client-api-test/src/lib.rs
@@ -1,11 +1,17 @@
 mod client;
 mod database_util;
 mod log;
+
 mod test_client;
 mod test_client_v2;
 mod user;
 
 pub use client::*;
 pub use log::*;
-pub use test_client::*;
 pub use user::*;
+
+#[cfg(not(feature = "v2"))]
+pub use test_client::*;
+
+#[cfg(feature = "v2")]
+pub use test_client_v2::*;

7585bbacb8b2a12b9604d0c17f1f7d3ae7ae7ce4
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2025-04-08 12:31:27 +0200

    chore: test client v2

--- tests/collab/missing_update_test.rs
+++ tests/collab/missing_update_test.rs
@@ -11,7 +11,7 @@ use uuid::Uuid;
 async fn client_apply_update_find_missing_update_test() {
   let (_client_1, mut client_2, object_id, mut expected_json) = make_clients().await;
   // "title" => "hello world" is not delivered to client_2 and is considered a missing update
-  client_2.ws_client.enable_receive_message();
+  client_2.enable_receive_message();
   {
     let mut lock = client_2
       .collabs
@@ -35,7 +35,7 @@ async fn client_apply_update_find_missing_update_test() {
 async fn client_ping_find_missing_update_test() {
   let (_client_1, mut client_2, object_id, expected_json) = make_clients().await;
   // "title" => "hello world" is not delivered to client_2 and is considered a missing update
-  client_2.ws_client.enable_receive_message();
+  client_2.enable_receive_message();
 
   // the collab ping will trigger a init sync with reason InitSyncReason::MissUpdates after a period of time
   assert_client_collab_include_value(&mut client_2, &object_id, expected_json)
@@ -67,7 +67,7 @@ async fn make_clients() -> (TestClient, TestClient, Uuid, Value) {
     .wait_object_sync_complete(&object_id)
     .await
     .unwrap();
-  client_2.ws_client.disable_receive_message();
+  client_2.disable_receive_message();
 
   // Client_1 makes the first edit by inserting "task 1".
   {

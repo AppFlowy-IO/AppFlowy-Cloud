0691bcaafc12066dc7a1cd9fa8026af33f8e0e25
Nathan <nathan@appflowy.io>
2025-04-12 10:44:21 +0800

    chore: fix test

--- tests/search/document_search.rs
+++ tests/search/document_search.rs
@@ -48,7 +48,7 @@ async fn test_embedding_when_create_document() {
   // The number of returned documents affected by the max token size when splitting the document
   // into chunks.
   assert_eq!(search_resp.items.len(), 2);
-  assert_eq!(search_resp.summaries.len(), 1);
+
   let previews = search_resp
     .items
     .iter()

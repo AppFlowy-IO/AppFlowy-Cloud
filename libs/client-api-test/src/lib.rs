892d826d36dd4396d49cd74d724bdc9ef98fbddc
Nathan <nathan@appflowy.io>
2025-05-24 12:58:02 +0800

    test: v2 client funtions

--- libs/client-api-test/src/lib.rs
+++ libs/client-api-test/src/lib.rs
@@ -1,12 +1,23 @@
 mod client;
 mod database_util;
 mod log;
-
 mod user;
+
+// New modules for better organization
+mod test_client_config;
+mod workspace_ops;
+mod async_utils;
+mod assertion_utils;
 
 pub use client::*;
 pub use log::*;
 pub use user::*;
+
+// Export new modules
+pub use test_client_config::*;
+pub use workspace_ops::*;
+pub use async_utils::*;
+pub use assertion_utils::*;
 
 #[cfg(not(feature = "v2"))]
 mod test_client;

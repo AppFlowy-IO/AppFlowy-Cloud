3ab0bf583a51818f4274aa7b87c6c385887fdcc3
Nathan <nathan@appflowy.io>
2025-05-29 09:32:08 +0800

    chore: bump collab commit id

--- libs/client-api-test/src/database_util.rs
+++ libs/client-api-test/src/database_util.rs
@@ -1,16 +1,11 @@
 use async_trait::async_trait;
-use collab::core::collab::CollabOptions;
-use collab::core::origin::CollabOrigin;
 use collab::entity::EncodedCollab;
-use collab::preclude::{ClientID, Collab};
+use collab::preclude::ClientID;
 use collab_database::error::DatabaseError;
-use collab_database::workspace_database::{
-  DatabaseCollabPersistenceService, DatabaseCollabService, EncodeCollabByOid,
-};
+use collab_database::workspace_database::{DatabaseCollabReader, EncodeCollabByOid};
 use collab_entity::CollabType;
 use database_entity::dto::QueryCollabResult::{Failed, Success};
 use database_entity::dto::{QueryCollab, QueryCollabParams};
-use std::sync::Arc;
 use tracing::error;
 use uuid::Uuid;
 
@@ -21,50 +16,33 @@ pub struct TestDatabaseCollabService {
 }
 
 #[async_trait]
-impl DatabaseCollabService for TestDatabaseCollabService {
+impl DatabaseCollabReader for TestDatabaseCollabService {
   async fn client_id(&self) -> ClientID {
     self.client_id
   }
 
-  async fn build_collab(
+  async fn get_collab(
     &self,
     object_id: &str,
-    object_type: CollabType,
-    encoded_collab: Option<(EncodedCollab, bool)>,
-  ) -> Result<Collab, DatabaseError> {
-    let encoded_collab = match encoded_collab {
-      None => {
-        let params = QueryCollabParams {
-          workspace_id: self.workspace_id,
-          inner: QueryCollab {
-            object_id: object_id.parse()?,
-            collab_type: object_type,
-          },
-        };
-        self
-          .api_client
-          .get_collab(params)
-          .await
-          .unwrap()
-          .encode_collab
+    collab_type: CollabType,
+  ) -> Result<EncodedCollab, DatabaseError> {
+    let object_id = Uuid::parse_str(object_id)?;
+    let params = QueryCollabParams {
+      workspace_id: self.workspace_id,
+      inner: QueryCollab {
+        object_id,
+        collab_type,
       },
-      Some((encoded_collab, _)) => encoded_collab,
     };
-    let options = CollabOptions::new(object_id.to_string(), self.client_id)
-      .with_data_source(encoded_collab.into());
-    Ok(Collab::new_with_options(CollabOrigin::Empty, options).unwrap())
+    let resp = self
+      .api_client
+      .get_collab(params)
+      .await
+      .map_err(|err| DatabaseError::Internal(err.into()))?;
+    Ok(resp.encode_collab)
   }
 
-  async fn finalize_collab(
-    &self,
-    _object_id: Uuid,
-    _collab_type: CollabType,
-    _collab: &mut Collab,
-  ) -> Result<(), DatabaseError> {
-    Ok(())
-  }
-
-  async fn get_collabs(
+  async fn batch_get_collabs(
     &self,
     object_ids: Vec<String>,
     collab_type: CollabType,
@@ -80,7 +58,7 @@ impl DatabaseCollabService for TestDatabaseCollabService {
       .api_client
       .batch_get_collab(&self.workspace_id, params)
       .await
-      .unwrap();
+      .map_err(|err| DatabaseError::Internal(err.into()))?;
     Ok(
       results
         .0
@@ -101,9 +79,5 @@ impl DatabaseCollabService for TestDatabaseCollabService {
         })
         .collect::<EncodeCollabByOid>(),
     )
-  }
-
-  fn persistence(&self) -> Option<Arc<dyn DatabaseCollabPersistenceService>> {
-    None
   }
 }

de3eed4cdb09e50232db248066ba2872ab72ab76
khorshuheng <solemnpriest@gmail.com>
2025-06-05 15:56:00 +0800

    feat: use post for shared view details

--- libs/shared-entity/src/dto/guest_dto.rs
+++ libs/shared-entity/src/dto/guest_dto.rs
@@ -27,7 +27,7 @@ pub struct SharedViewDetails {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct QuerySharedViewDetailsParams {
+pub struct SharedViewDetailsRequest {
   pub ancestor_view_ids: Vec<Uuid>,
 }
 
